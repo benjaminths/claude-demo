6170d3ca5728cb917a4e7c09b6ffa2b101d6aa83
Benjamin <benjamint@okeenea.com>
2025-10-23 15:29:46 +0200

    Initial Commit

---
+++ ClaudeDemo/ClaudeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeDemoApp.swift
+//  ClaudeDemo
+//
+//  Created by Benjamin on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeDemoTests/ClaudeDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClaudeDemoTests.swift
+//  ClaudeDemoTests
+//
+//  Created by Benjamin on 23/10/2025.
+//
+
+import Testing
+
+struct ClaudeDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
